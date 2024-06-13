24cdddb373ad2c5fdfd33d2aa748fb210b1e25ab
Nelson025 <145264544+Nelson025@users.noreply.github.com>
2024-06-13 20:58:27 +0800

    Add files via upload

---
+++ PE_T1D_01_expouese.R
@@ -0,0 +1,84 @@
+#install.packages("devtools")
+#devtools::install_github("mrcieu/gwasglue", force = TRUE)
+
+#if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("VariantAnnotation")
+
+#install.packages("dplyr")
+#install.packages("tidyr")
+#install.packages("CMplot")
+
+
+#引用包
+library(VariantAnnotation)
+library(gwasglue)
+library(dplyr)
+library(tidyr)
+library(CMplot)
+library(TwoSampleMR)
+
+setwd("F:\\先兆子痫_糖尿病\\T1D\\01.exposure")      #设置工作目录
+
+#############################################04.exposure----
+#读取输入文件, 并对输入文件进行格式转换
+#### 1.加载结局数据 ----
+load('finndataR10_Preeclampsia_exposure.rdata')
+data=finndata
+
+#根据pvalue<5e-08对结果进行过滤
+outTab<-subset(data, pval.exposure<5e-08)
+write.csv(outTab, file="exposure.pvalue.csv", row.names=F)
+
+#准备绘制曼哈顿图的数据
+data=data[,c("SNP", "chr.exposure", "pos.exposure", "pval.exposure")]
+colnames(data)=c("SNP","CHR","BP","pvalue")
+
+#绘制线性的曼哈顿图
+CMplot(data,  plot.type="m",
+       LOG10=TRUE, threshold=5e-08, threshold.lwd=3, threshold.lty=1, signal.cex=0.2,
+       chr.den.col=NULL, cex=0.2, bin.size=1e5, ylim=c(0,50),
+       file="pdf", file.output=TRUE, width=15, height=9, verbose=TRUE)
+
+#绘制圈图
+CMplot(data,  plot.type="c",
+       LOG10=TRUE, threshold=5e-08, threshold.lwd=3, threshold.lty=1, signal.cex=0.2,
+       chr.den.col=NULL, cex=0.2, bin.size=1e5, ylim=c(0,100),
+       file="pdf", file.output=TRUE, width=7, height=7, verbose=TRUE)
+
+#############################################05.DL----
+#去除连锁不平衡的SNP
+exposureFile="exposure.pvalue.csv"     #输入文件
+
+#读取输入文件
+exposure_dat<-read_exposure_data(filename=exposureFile,
+                                 sep = ",",
+                                 snp_col = "SNP",
+                                 beta_col = "beta.exposure",
+                                 se_col = "se.exposure",
+                                 effect_allele_col = "effect_allele.exposure",
+                                 other_allele_col = "other_allele.exposure",
+                                 eaf_col = "eaf.exposure",
+                                 samplesize_col = "samplesize.exposure",
+                                 clump = F)
+
+#去除连锁不平衡的SNP
+exposure_dat_clumped <- clump_data(exposure_dat, clump_kb=10000, clump_r2=0.001)
+write.csv(exposure_dat_clumped, file="exposure.LD.csv", row.names=F)
+
+#############################################06.F----
+Ffilter=10        #F值过滤条件
+inputFile="exposure.LD.csv"      #输入文件
+
+#读取输入文件
+dat<-read.csv(inputFile, header=T, sep=",", check.names=F)
+
+#计算F检验值
+N=dat[1,"samplesize.exposure"]     #获取样品的数目
+dat=transform(dat,R2=2*((beta.exposure)^2)*eaf.exposure*(1-eaf.exposure))     #计算R2
+dat=transform(dat,F=(N-2)*R2/(1-R2))      #计算F检验值
+
+#根据F值>10进行过滤, 删除弱工具变量
+outTab=dat[dat$F>Ffilter,]
+write.csv(outTab, "exposure.F.csv", row.names=F)
+
---
+++ T2D_PE_02_outcomeMR.R
@@ -0,0 +1,95 @@
+setwd("F:\\糖尿病_先兆子痫\\R10\\T2D\\02.TwoSampleMR")      #设置工作目录
+
+#### 1.加载结局数据 ----
+load('finndataR10_preeclampsia_outcome.rdata')
+
+#### 2.加载暴露数据 ----
+#引用包
+library(VariantAnnotation)
+library(gwasglue)
+library(TwoSampleMR)
+
+exposureName=" Type 2 diabetes"                         #图形中展示暴露数据的名称
+outcomeName="preeclampsia"       #图形中展示结局数据的名称
+
+exposureFile="exposure.F.csv"     #暴露数据输入文件
+
+
+#读取暴露数据
+exposure_dat<-read_exposure_data(filename=exposureFile,
+                                 sep = ",",
+                                 snp_col = "SNP",
+                                 beta_col = "beta.exposure",
+                                 se_col = "se.exposure",
+                                 effect_allele_col = "effect_allele.exposure",
+                                 other_allele_col = "other_allele.exposure",
+                                 eaf_col = "eaf.exposure",
+                                 clump = F)
+
+#### 3.从结局数据中提取工具变量 ----
+outcomeTab<-merge(exposure_dat, finndata, by.x="SNP", by.y="SNP")
+write.csv(outcomeTab[,-(2:ncol(exposure_dat))], file="outcome.csv") 
+
+#### 4.读取整理好的结局数据  ----
+outcome_dat<-read_outcome_data(snps=exposure_dat$SNP,
+                               filename="outcome.csv", sep = ",",
+                               snp_col = "SNP",
+                               beta_col = "beta.outcome",
+                               se_col = "se.outcome",
+                               effect_allele_col = "effect_allele.outcome",
+                               other_allele_col = "other_allele.outcome",
+                               pval_col = "pval.outcome",
+                               eaf_col = "eaf.outcome")
+
+#### 5.将暴露数据和结局数据合并 ----
+exposure_dat$exposure=exposureName
+outcome_dat$outcome=outcomeName
+dat<-harmonise_data(exposure_dat=exposure_dat,
+                    outcome_dat=outcome_dat)
+
+#输出用于孟德尔随机化的工具变量
+outTab=dat[dat$mr_keep=="TRUE",]
+write.csv(outTab, file="table.SNP.csv", row.names=F)
+
+#MR-PRESSO异常值检测(偏倚的SNP)
+presso=run_mr_presso(dat)
+write.csv(presso[[1]]$`MR-PRESSO results`$`Outlier Test`, file="table.MR-PRESSO.csv")
+
+#孟德尔随机化分析
+mrResult=mr(dat)
+#选择孟德尔随机化的方法
+#mr_method_list()$obj
+#mrResult1=mr(dat, method_list=c("mr_ivw", "mr_egger_regression", "mr_weighted_median", "mr_simple_mode", "mr_weighted_mode"))
+#对结果进行OR值的计算
+mrTab=generate_odds_ratios(mrResult)
+write.csv(mrTab, file="table.MRresult.csv", row.names=F)
+
+#异质性分析
+heterTab=mr_heterogeneity(dat)
+write.csv(heterTab, file="table.heterogeneity.csv", row.names=F)
+
+#多效性检验
+pleioTab=mr_pleiotropy_test(dat)
+write.csv(pleioTab, file="table.pleiotropy.csv", row.names=F)
+
+#绘制散点图
+pdf(file="pic.scatter_plot.pdf", width=7.5, height=7)
+mr_scatter_plot(mrResult, dat)
+dev.off()
+
+#森林图
+res_single=mr_singlesnp(dat)      #得到每个工具变量对结局的影响
+pdf(file="pic.forest.pdf", width=7, height=6.5)
+mr_forest_plot(res_single)
+dev.off()
+
+#漏斗图
+pdf(file="pic.funnel_plot.pdf", width=7, height=6.5)
+mr_funnel_plot(singlesnp_results = res_single)
+dev.off()
+
+#留一法敏感性分析
+pdf(file="pic.leaveoneout.pdf", width=7, height=6.5)
+mr_leaveoneout_plot(leaveoneout_results = mr_leaveoneout(dat))
+dev.off()
+
